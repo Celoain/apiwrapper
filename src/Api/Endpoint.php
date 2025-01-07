8c1170649fc1e46eaa41357abde4c82cb19bcde7
celoain <mju76zhn>
2025-01-07 15:36:19 +0100

    Adding docker files to check github actions

--- src/Api/Endpoint.php
+++ src/Api/Endpoint.php
@@ -2,43 +2,9 @@
 
 namespace Celoain\ApiWrapper\Api;
 
-use /**
- * Interface EndpointInterface
- * An interface for defining API endpoints within the AsanaHub application.
- * The implementing class will define methods for interacting with specific API endpoints,
- * utilizing the application's database connection and queue system.
- *
- * @package Celoain\ApiWrapper\Contracts\Api
- */
-    Celoain\ApiWrapper\Contracts\Api\EndpointInterface;
-use /**
- * ProcessorInterface is a contract for handling API processing tasks
- * in the AsanaHub Laravel application.
- * This interface defines the core methods required for processing
- * API requests and responses, ensuring consistent implementation
- * across different processor classes.
- * Implementing classes are expected to interact with a MySQL database
- * and handle queued tasks using a database connection in a Laravel
- * environment.
- */
-    Celoain\ApiWrapper\Contracts\Api\ProcessorInterface;
-use /**
- * Enumeration of HTTP methods used in API requests within the AsanaHub Laravel application.
- * This enum provides a standardized set of HTTP method constants to be used when
- * making API requests. It facilitates consistent and error-free usage of HTTP methods
- * in the application.
- * Supported HTTP methods include:
- * - GET
- * - POST
- * - PUT
- * - DELETE
- * - PATCH
- * - OPTIONS
- * - HEAD
- * Each method corresponds to a commonly used HTTP verb, providing a clear understanding
- * of the intended action to be performed on the resource.
- */
-    Celoain\ApiWrapper\Enums\Api\HttpMethods;
+use Celoain\ApiWrapper\Contracts\Api\EndpointInterface;
+use Celoain\ApiWrapper\Contracts\Api\ProcessorInterface;
+use Celoain\ApiWrapper\Enums\Api\HttpMethods;
 
 class Endpoint implements EndpointInterface
 {
